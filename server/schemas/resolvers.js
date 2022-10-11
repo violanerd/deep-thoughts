1ecf245e014e6a1d20432dfbf39fad015d7e3e27
violanerd <madalyne.cross@gmail.com>
2022-10-11 13:54:33 -0500

    added mutations and JWT

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
     Query: {
@@ -21,6 +23,29 @@ const resolvers = {
             .select('-__v -password')
             .populate('friends')
             .populate('thoughts')
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return {user, token};
+        },
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+
+            if (!user){
+                throw new AuthenticationError('Incorrect Credentials')
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw){
+                throw new AuthenticationError('Incorrect Credentials')
+            }
+
+            const token = signToken(user);
+            return {user, token};
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,7 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
+
 //create our typeDefs
 const typeDefs = gql`
 type Thought {
@@ -30,6 +31,14 @@ type Query {
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+}
+type Auth {
+    token: ID!
+    user: User
 }`;
 
 module.exports = typeDefs;
