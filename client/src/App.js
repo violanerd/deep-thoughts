30598bd71bcabd977548e48de6354c131e34cc00
violanerd <madalyne.cross@gmail.com>
2022-10-13 16:02:12 -0500

    added authentication and user nav

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,23 @@ import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql'
 })
+//first argument underscore is a placeholder; not using first param, a store of the req object
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+})
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 function App() {
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
+import Auth from '../utils/auth'; 
+import FriendList from '../components/Friends'
 
 const Home = () => {
   //use useQuery to make a query request
   const {loading, data} = useQuery(QUERY_THOUGHTS)
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
   const thoughts = data?.thoughts || []
   console.log(thoughts)
+  // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
+  const loggedIn = Auth.loggedIn();
+
+
+  
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
@@ -19,6 +27,15 @@ const Home = () => {
           )
 
         }</div>
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+  </div>
+) : null}
       </div>
     </main>
   );
